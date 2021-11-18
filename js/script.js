cc15f71ee0b7f172e4de88b51c3cd449e666a4d4
広田響 <hirotahibiki@hibiki.local>
2021-11-18 22:14:03 +0900

    toppage create.

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
-
 // ハンバーガーメニュー
 jQuery('.drawer_icon').on('click', function(e) {
   e.preventDefault();
@@ -34,4 +33,30 @@ jQuery('.drawer_icon').on('click', function(e) {
   jQuery('.footer_fixd').toggleClass('is_active');
 
   return false
+});
+
+// スムーススクロール
+jQuery(function() {
+  var appear = false;
+  var pagetop = $('.to_top');
+  $(window).scroll(function () {
+    if (200 < jQuery(this).scrollTop()) {
+      jQuery('.to_top').addClass('is_show')
+      pagetop.fadeIn(1000);
+    } else {
+      jQuery('.to_top').removeClass('is_show')
+      pagetop.fadeOut(1000);
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+});
+
+
+// is_activeをつける
+jQuery('.header_nav .header_list .header_item li a .header_image svg ').on('click', function() {
+  jQuery('.header_nav .header_item .header_image svg li a').removeClass('is_active');
+  jQuery(this).addClass('is_active')
 });
